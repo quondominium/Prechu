6acbac160a72cf79dcffe4342418df989b999d96
quondominium <quondominium@gmail.com>
2018-05-14 10:27:12 +0300

    js

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+			  function listen() {
+			    var HAMBURGER_ACTIVE = 'is-active';
+			    var NAVIGATION_OPENED = 'is-navigation-opened';
+			    var MOBILE_RESOLUTION = 767;
+
+			    var body = document.querySelector('body');
+			    var btnHamburger = document.querySelector('.btn-hamburger');
+			    btnHamburger.addEventListener('click', function () {
+			      btnHamburger.classList.toggle(HAMBURGER_ACTIVE);
+			      body.classList.toggle(NAVIGATION_OPENED);
+			    });
+
+			    window.addEventListener('resize', function(){
+			      if(window.innerWidth > MOBILE_RESOLUTION){
+			        btnHamburger.classList.remove(HAMBURGER_ACTIVE);
+			        body.classList.remove(NAVIGATION_OPENED);	        
+			      }
+			    });
+			  }
+
+			  listen();
---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+		  		var mySwiper = new Swiper ('.swiper-container', {
+	/*      			loop: true,
+	      			autoplay: {
+	        		delay: 10000,
+	        		disableOnInteraction: false,
+	      			},*/
+		    		navigation: {
+		      		nextEl: '.swiper-button-next',
+		      		prevEl: '.swiper-button-prev',
+		    		},
+		  		})
